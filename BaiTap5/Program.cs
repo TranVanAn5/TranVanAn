c8505fdba154e87fdd1ca8f1d505a6f32f7b25f5
TranVanAn <antvse181691@fpt.edu.vn>
2024-10-26 01:25:07 +0700

    BaiTap5

---
+++ BaiTap5/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nhập vào số tiền bạn cần đổi (USD): ");
+String? tien = Console.ReadLine();
+
+double usd = Convert.ToDouble(tien);
+
+double vnd = usd * 25384;
+
+Console.WriteLine("---------------(1 USD = 25384 VND)------------------" );
+Console.WriteLine("Số tiền sau khi được chuyển đổi là (USD to VND): " + vnd +" VND" );
