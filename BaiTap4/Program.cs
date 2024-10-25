788ad68986a6b1c9242252ccf438e2c53c403c9f
TranVanAn <antvse181691@fpt.edu.vn>
2024-10-26 01:11:31 +0700

    BaiTap4

---
+++ BaiTap4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nhập vào số tiền gốc: ");
+String? tien = Console.ReadLine();
+Console.WriteLine("nhập vào phí VAT: " );
+String? vat = Console.ReadLine();
+
+double tienGoc = Convert.ToDouble(tien);
+int phiVAT = Convert.ToInt32(vat);
+
+double tienPhi = tienGoc * phiVAT / 100;
+double tongTien = tienGoc + tienPhi;
+
+Console.WriteLine("Tổng số tiền sau phí là: " + tongTien);
