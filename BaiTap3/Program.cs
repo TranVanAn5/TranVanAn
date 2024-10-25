2108f6aec8dad39c9ebf9bb277919355c07725e2
TranVanAn <antvse181691@fpt.edu.vn>
2024-10-26 00:59:27 +0700

    BaiTap3

---
+++ BaiTap3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nhập vào số phút: ");
+String? phut = Console.ReadLine();
+
+int minute = Convert.ToInt32(phut);
+
+int soGio = minute / 60;
+int soPhut = minute % 60;
+
+Console.WriteLine("Thời gian của bạn là " + soGio + " Giờ và " + soPhut + " Phút." );
