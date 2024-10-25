0c580f891cebdf1cf1e66840c7b9db82a0670ae4
TranVanAn <antvse181691@fpt.edu.vn>
2024-10-26 00:42:19 +0700

    BaiTap1

---
+++ BaiTap1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nhập Vào số ngày còn lại là: ");
+String? ngay = Console.ReadLine();
+
+int day = Convert.ToInt32(ngay);
+
+int soTuan = day / 7;
+int soNgay = day % 7;
+
+Console.WriteLine("Còn lại " + soTuan + " Tuần và " +soNgay + " Ngày" );
+
+
